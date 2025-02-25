f524324dbb7b2db655904fad1c1a33a3dbb07517
XinyuXiang <69185663+XinyuXiang@users.noreply.github.com>
2025-02-25 11:09:44 +0800

    Update 05_RepeatmaskerRound2.sh

--- TransposonAnnotation/05_RepeatmaskerRound2.sh
+++ TransposonAnnotation/05_RepeatmaskerRound2.sh
@@ -7,7 +7,7 @@ Refgenome=./01_simple_out/AmexG_v6.0-DD.simple.masked.fasta
 consensus_merge_fa=/0231101axolotl_TEanno/04_RepeatMasker/AmexG_v6.0_consensus.known.merge.fa 
 outdir=02_known_out
 
-# round 2: annotate/mask Tetrapoda elements sourced from Repbase using output from 1st round of RepeatMasker
+# round 2: annotate known consensus using output from round 1
 export BLAST_USAGE_REPORT=false # to accelerate rmblast when run >1 week
 
 ${RepeatMasker_path}RepeatMasker -pa 32 -e rmblast -a -dir ${outdir} -gff \
