0a0b9fd02ca054dcd391fe91286790a961672ca6
XinyuXiang <69185663+XinyuXiang@users.noreply.github.com>
2025-02-25 10:52:22 +0800

    Update 03_DeepTE.sh

--- TransposonAnnotation/03_DeepTE.sh
+++ TransposonAnnotation/03_DeepTE.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 conda activate DeepTE
 
-# Split known and unknown 
+# setup
+out_dir=/20231101axolotl_TEanno/03_DeepTE/
+DeepTE_path=/biosoft/RepeatMasker_related/DeepTE/
+model_dir=/biosoft/RepeatMasker_related/DeepTE/Metazoans_model/
 
+# Split known and unknown 
+cat AmexG_v6.0-families.fa | seqkit fx2tab | grep -v "Unknown" | seqkit tab2fx > AmexG_v6.0_consensus.known.fa
+cat AmexG_v6.0-families.fa | seqkit fx2tab | grep "Unknown" | seqkit tab2fx > AmexG_v6.0_consensus.unknown.fa
+
+# annotate unknown
+input_fasta=/20231101axolotl_TEanno/01_RepeatModeler/AmexG_v6.0_consensus.unknown.fa
+python ${DeepTE_path}DeepTE.py -d ${out_dir} -o ${out_dir} -i ${input_fasta} -sp M -m_dir ${model_dir} >& ./logs/AmexG_v6.0.log
